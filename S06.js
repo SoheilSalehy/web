08f8b10adefc30b8d3c817c1084850406e26ff08
SoheilSalehy <solyacount@gmail.com>
2023-11-07 05:28:04 -0800

    S06

---
+++ S06.js
@@ -0,0 +1,29 @@
+let http = require ('http');
+let server = http.createServer(requestHandler);
+server.listen(80);
+console.log("server is running on port:" + port)
+let headers = {'contact-type': 'text-/plain'}
+let obj = {
+    x: function(){
+        console.log('xxxx')        
+    },
+    y: function(){
+        console.log('yyyyyy')
+    },
+    "Favico": function(){
+        console.log('Favicon')
+    }
+}
+
+
+ function requestHandler (request,response)
+ {
+    //console.log('request url:',request.url);
+    //console.log('request method:',request.method);
+    let firstpage = request.url.split('/')[1];
+    obj[firstpage]();
+
+    response.writeHead(200,headers);
+    response.write('Salam');
+    response.end();
+ }
